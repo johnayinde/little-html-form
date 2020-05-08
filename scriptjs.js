9541df5031067e29c2290511318e14c6d675315d
Ayinde John <38318744+johnayinde@users.noreply.github.com>
2020-05-08 02:21:07 +0100

    first commit

---
+++ scriptjs.js
@@ -0,0 +1,9 @@
+tap = () => {
+    let z = document.form.title.value
+    let a = document.form.user.value
+    let x = document.getElementById('#submit')
+    
+    alert(` Hey ${z} ${a} this form is under construction 
+    			Thanks`)
+     //alert("Hey, " + z + " " + a + " This Form is under Construction Thanks")
+}
